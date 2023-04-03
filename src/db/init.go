d28ad7e236cf4618a277df0971668d1c9add2ddf
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-03 14:35:05 +0900

    Implement migration and etc.

--- src/db/init.go
+++ src/db/init.go
@@ -1,11 +1,29 @@
 package db
 
 import (
+	"context"
 	"fmt"
+	"github.com/kreimben/ZeroTwo_bot/src/db/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
 	"os"
+	"time"
 )
+
+var DbLogger = logger.New(
+	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+	logger.Config{
+		SlowThreshold:             time.Second, // Slow SQL threshold
+		LogLevel:                  logger.Info, // Log level
+		IgnoreRecordNotFoundError: true,
+		Colorful:                  true,
+	},
+)
+
+var DB *gorm.DB
+var err error
 
 func DbInit() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -15,9 +33,34 @@ func DbInit() {
 		os.Getenv("DB_NAME"),
 	)
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: DbLogger,
+	})
 	if err != nil {
 		fmt.Printf("failed to connect database: %s", err)
 		panic("failed to connect database")
 	}
+
+	// Automatically migrate the schema
+	// It WON’T delete unused columns to protect your data.
+	err = Migrate([]interface{}{
+		&models.CommandHistory{},
+		&models.YoutubeHistory{},
+	})
+	if err != nil {
+		fmt.Printf("failed to migrate database: %s", err)
+		panic("failed to migrate database")
+	}
+}
+
+func Migrate(models []interface{}) error {
+	for _, model := range models {
+		DbLogger.Info(context.Background(), "Migrating model: "+fmt.Sprintf("%T", model))
+		err = DB.AutoMigrate(model)
+		if err != nil {
+			return err
+		}
+	}
+	log.Println("Completed Migrate")
+	return nil
 }
