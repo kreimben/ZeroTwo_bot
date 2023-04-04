e84607eeebbbc00c8baa8874c1c2b38ebbb1b22e
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-04 14:36:19 +0900

    Refactoring.

--- src/server/auth.go
+++ src/server/auth.go
@@ -81,41 +81,6 @@ func (s *discordServer) RefreshAccessToken(_ context.Context, req *gen.RefreshAc
 	return convertBodyToJSON(res.Body)
 }
 
-func convertBodyToJSON(body io.ReadCloser) (*gen.LoginWithDiscordResponse, error) {
-	// Convert response to JSON.
-	rawJSON, _ := io.ReadAll(body)
-	var (
-		token discordAccessToken
-		e     discordError
-	)
-	log.Println("rawJSON: " + string(rawJSON))
-	if strings.Contains(string(rawJSON), "access_token") {
-		if err := json.Unmarshal(rawJSON, &token); err != nil {
-			GRPCLogger.Println("Couldn't parse JSON: " + err.Error())
-			return nil, err
-		}
-		return &gen.LoginWithDiscordResponse{
-			Response: &gen.LoginWithDiscordResponse_AccessToken{
-				AccessToken: &gen.DiscordAccessTokenResponse{
-					AccessToken:  token.AccessToken,
-					ExpiresIn:    int64(token.ExpiresIn),
-					RefreshToken: token.RefreshToken,
-					Scope:        token.Scope,
-					TokenType:    token.TokenType,
-				}},
-		}, nil
-	} else if strings.Contains(string(rawJSON), "error") {
-		if err := json.Unmarshal(rawJSON, &e); err != nil {
-			GRPCLogger.Println("Couldn't parse JSON: " + err.Error())
-			return nil, err
-		}
-		return &gen.LoginWithDiscordResponse{
-			Response: &gen.LoginWithDiscordResponse_Error{
-				Error: &gen.DiscordErrorResponse{
-					Error:            e.Error,
-					ErrorDescription: e.ErrorDescription,
-				}},
-		}, nil
-	}
-	return nil, errors.New("Unknown error: " + string(rawJSON))
+func (s *discordServer) GetMyInfo(_ context.Context, req *gen.GetMyInfoRequest) (*gen.GetMyInfoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMyInfo not implemented")
 }
---
+++ src/server/helper.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	gen "github.com/kreimben/ZeroTwo_bot/src/gen"
+	"io"
+	"log"
+	"strings"
+)
+
+func convertBodyToJSON(body io.ReadCloser) (*gen.LoginWithDiscordResponse, error) {
+	// Convert response to JSON.
+	rawJSON, _ := io.ReadAll(body)
+	var (
+		token discordAccessToken
+		e     discordError
+	)
+	log.Println("rawJSON: " + string(rawJSON))
+	if strings.Contains(string(rawJSON), "access_token") {
+		if err := json.Unmarshal(rawJSON, &token); err != nil {
+			GRPCLogger.Println("Couldn't parse JSON: " + err.Error())
+			return nil, err
+		}
+		return &gen.LoginWithDiscordResponse{
+			Response: &gen.LoginWithDiscordResponse_AccessToken{
+				AccessToken: &gen.DiscordAccessTokenResponse{
+					AccessToken:  token.AccessToken,
+					ExpiresIn:    int64(token.ExpiresIn),
+					RefreshToken: token.RefreshToken,
+					Scope:        token.Scope,
+					TokenType:    token.TokenType,
+				}},
+		}, nil
+	} else if strings.Contains(string(rawJSON), "error") {
+		if err := json.Unmarshal(rawJSON, &e); err != nil {
+			GRPCLogger.Println("Couldn't parse JSON: " + err.Error())
+			return nil, err
+		}
+		return &gen.LoginWithDiscordResponse{
+			Response: &gen.LoginWithDiscordResponse_Error{
+				Error: &gen.DiscordErrorResponse{
+					Error:            e.Error,
+					ErrorDescription: e.ErrorDescription,
+				}},
+		}, nil
+	}
+	return nil, errors.New("Unknown error: " + string(rawJSON))
+}
