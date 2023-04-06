fc06e453a668139b0b884e3b6b7e676c5c6b1d74
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-06 17:34:21 +0900

    Move feature to test.

--- src/discord/commands/hey.go
+++ src/discord/commands/hey.go
@@ -16,53 +16,6 @@ var heyCommand = &discordgo.ApplicationCommand{
 }
 
 func heyHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// Check if the user is in a voice channel
-	voiceState, err := s.State.VoiceState(i.GuildID, i.Member.User.ID) // user's voice state
-	if err != nil {
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "You need to be in a voice channel to use this command!",
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			log.Println("Error sending response: " + err.Error())
-			return
-		}
-		return
-	}
-
-	// If voice client is not in a channel, join the user's channel
-	if s.VoiceConnections == nil || s.VoiceConnections[i.GuildID] == nil {
-		// Join the channel
-		_, err := s.ChannelVoiceJoin(i.GuildID, voiceState.ChannelID, false, false)
-		if err != nil {
-			log.Println("Error joining voice channel: " + err.Error())
-			if err != nil {
-				log.Println("Error joining voice channel: " + err.Error())
-				return
-			}
-			return
-		}
-	} else {
-		// move to the user's channel
-		join, err := s.ChannelVoiceJoin(voiceState.GuildID, voiceState.ChannelID, false, false)
-		if err != nil {
-			log.Println("Error joining voice channel: " + err.Error())
-			if err != nil {
-				log.Println("Error joining voice channel: " + err.Error())
-				return
-			}
-			return
-		}
-		err = join.Speaking(true)
-		if err != nil {
-			log.Println("Error speaking: " + err.Error())
-			return
-		}
-	}
-
 	// before sending the response, record to db
 	db.DatabaseSession.Create(&models.CommandHistory{
 		GuildID:  i.GuildID,
