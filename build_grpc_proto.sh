2415800fd44477f2af5db2da89e08d44face302b
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-09 23:43:15 +0900

    Add typescript version.

--- build_grpc_proto.sh
+++ build_grpc_proto.sh
@@ -1,11 +1,19 @@
 # shellcheck disable=SC2046
 
 # golang generating
-echo `protoc --proto_path="./protobuf" \
---proto_path=protobuf \
---go_out=./src/gen \
---go_opt=paths=source_relative \
---go-grpc_out=./src/gen \
---go-grpc_opt=paths=source_relative \
---proto_path=paths=source_relative \
+#echo `protoc --proto_path="./protobuf" \
+#--proto_path=protobuf \
+#--go_out=./src/gen \
+#--go_opt=paths=source_relative \
+#--go-grpc_out=./src/gen \
+#--go-grpc_opt=paths=source_relative \
+#--proto_path=paths=source_relative \
+#protobuf/*.proto`
+
+# typescript generating for deno
+echo `protoc \
+--proto_path="./protobuf" \
+--plugin=protoc-gen-ts=/opt/homebrew/bin/protoc-gen-ts \
+--ts_out=./pages/src/gen \
+--grpc-web_out=import_style=typescript,mode=grpcwebtext:./pages/src/gen \
 protobuf/*.proto`
