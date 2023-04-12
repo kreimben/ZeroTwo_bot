93b86f310ca0d4d1bcbeef688cdf4d6f9a2339c8
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-12 18:41:46 +0900

    Fix for test.

--- src/discord/init.go
+++ src/discord/init.go
@@ -48,8 +48,8 @@ func DiscordInit() {
 	goutubedl.Path = os.Getenv("YT_DL_PATH")
 
 	// Register commands
-	//commands.RegisterHey(DiscordSession, guildId)
+	commands.RegisterHey(DiscordSession, guildId)
 	//commands.RegisterDance(DiscordSession, guildId)
 	//commands.RegisterVersion(DiscordSession, guildId)
-	commands.RegisterTest(DiscordSession, guildId) // Just test for playing music
+	//commands.RegisterTest(DiscordSession, guildId) // Just test for playing music
 }
