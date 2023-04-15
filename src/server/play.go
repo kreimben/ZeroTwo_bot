b6da189b010c6baa85110c67c8a8968dcd822c34
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-15 21:24:22 +0900

    Implement video search with keyword or url. Not support multiple results yet.

--- src/server/play.go
+++ src/server/play.go
@@ -2,38 +2,47 @@ package server
 
 import (
 	"context"
+	"github.com/golang/protobuf/ptypes/duration"
 	gen "github.com/kreimben/ZeroTwo_bot/src/gen"
+	"github.com/kreimben/youtube-info-extractor/video"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (p *playerServer) Search(_ context.Context, req *gen.SearchRequest) (*gen.SearchResponse, error) {
-	//results, err := ytSearch.Q(req.GetKeyword()).MaxResults(int64(req.GetAmount())).Do()
-	//if err != nil {
-	//	GRPCLogger.Println("Error making search API call: %v", err)
-	//	return nil, err
-	//}
-	//
-	arr := make([]*gen.VideoInfo, 0)
-	//for _, item := range results.Items {
-	//	log.Println(item.Snippet.Title)
-	//
-	//	// to get a duration of videos.
-	//	videoCall := ytService.Videos.List([]string{"contentDetails"}).Id(item.Id.VideoId)
-	//	videoResponse, err := videoCall.Do()
-	//	if err != nil {
-	//		log.Fatalf("Error making video API call: %v", err)
-	//	}
-	//
-	//	arr = append(arr, &gen.VideoInfo{
-	//		Title:        item.Snippet.Title,
-	//		Url:          string("https://youtube.com/watch&v=" + item.Id.VideoId),
-	//		ThumbnailUrl: item.Snippet.Thumbnails.High.Url,
-	//		Duration:     &duration.Duration{Seconds: int64(parseDuration(videoResponse.Items[0].ContentDetails.Duration))},
-	//	})
-	//}
+	videoCh := make(chan *video.Video)
 
-	return &gen.SearchResponse{VideoInfo: arr}, nil
+	if req.GetKeyword() != "" {
+		go video.SearchOneVideoKeyword(req.GetKeyword(), videoCh)
+		if v, ok := <-videoCh; ok {
+			var arr []*gen.VideoInfo
+			arr = append(arr, &gen.VideoInfo{
+				Title:        v.Title,
+				Url:          string("https://youtube.com/watch?v=" + v.Id),
+				ThumbnailUrl: v.ThumbnailUrl,
+				Duration:     &duration.Duration{Seconds: int64(v.Duration)},
+			})
+			return &gen.SearchResponse{VideoInfo: arr}, nil
+		} else { // that means not ok.
+			return nil, status.Errorf(codes.NotFound, "no video found")
+		}
+	} else if req.GetUrl() != "" {
+		go video.SearchOneVideoUrl(req.GetUrl(), videoCh)
+		if v, ok := <-videoCh; ok {
+			var arr []*gen.VideoInfo
+			arr = append(arr, &gen.VideoInfo{
+				Title:        v.Title,
+				Url:          string("https://youtube.com/watch?v=" + v.Id),
+				ThumbnailUrl: v.ThumbnailUrl,
+				Duration:     &duration.Duration{Seconds: int64(v.Duration)},
+			})
+			return &gen.SearchResponse{VideoInfo: arr}, nil
+		} else { // that means not ok.
+			return nil, status.Errorf(codes.NotFound, "no video found")
+		}
+	}
+
+	return nil, status.Errorf(codes.InvalidArgument, "no keyword or url")
 }
 
 func (p *playerServer) Play(_ context.Context, req *gen.PlayRequest) (*gen.PlayResponse, error) {
