8444165d590449227d536fc610b3c0b48a6358a5
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-17 23:07:32 +0900

    Implement card view currently where I came from.

---
+++ pages/src/components/CurrentConnectedGuildInfo.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const CurrentConnectedGuildInfo = ({guildName, guildIcon}) => {
+    return (
+        <CurrentConnectedGuildInfoWrapper>
+            <GuildName>{guildName}</GuildName>
+            <GuildImage src={guildIcon} alt={guildName}/>
+        </CurrentConnectedGuildInfoWrapper>
+    )
+}
+
+const GuildName = styled.div`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
+
+const GuildImage = styled.img`
+  height: 64px;
+  width: 64px;
+  border-radius: 50%;
+  margin-left: 16px;
+`;
+
+const CurrentConnectedGuildInfoWrapper = styled.div`
+  margin-top: 16px;
+  background-color: red;
+  height: 96px;
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* From https://css.glass */
+  background: rgba(216, 170, 53, 0.28);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border: 1px solid rgba(216, 170, 53, 0.3);
+`;
