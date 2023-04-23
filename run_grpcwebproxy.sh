3e607b5c3be6187b7be4c74a014e4e6bf4babe1c
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-23 21:54:15 +0900

    Fix program parameter

--- run_grpcwebproxy.sh
+++ run_grpcwebproxy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -x
 
-grpcwebproxy --backend_addr=localhost:5011 --allow_all_origins --run_tls_server=false --server_http_debug_port=5012
+grpcwebproxy --backend_addr=localhost:5011 \
+--allow_all_origins --run_tls_server=false --server_http_debug_port=5012 \
+--server_http_max_write_timeout=3600s \
+--server_http_max_read_timeout=3600s
