223db6356ba19d168adea1993af330a865353dc4
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-29 14:25:05 +0900

    Fix type error.

--- pages/src/types.ts
+++ pages/src/types.ts
@@ -1,9 +1,14 @@
-import { ItemInterface } from "react-sortablejs";
-import {Song} from "./gen/queue_pb";
+import {ItemInterface} from "react-sortablejs";
+import {Song} from "./gen/queue";
 
 class SongItem implements ItemInterface {
     id: number
     song: Song
+
+    constructor(id: number, song: Song) {
+        this.id = id;
+        this.song = song;
+    }
 }
 
 export {SongItem}
