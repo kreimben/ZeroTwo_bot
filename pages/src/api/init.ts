12eac2e535d125857c442bd22bda7e752447348e
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-30 21:41:50 +0900

    Split backend and frontend.

--- pages/src/api/init.ts
+++ pages/src/api/init.ts
@@ -1,6 +1,6 @@
 import {GrpcWebFetchTransport} from "@protobuf-ts/grpcweb-transport";
 
 // @ts-ignore
-const host = import.meta.env.VITE_GRPC_HOST || 'http://localhost:5012';
+const host = import.meta.env.VITE_GRPC_HOST || 'https://api.zerotwo.kreimben.com';
 const transport = new GrpcWebFetchTransport({baseUrl: host})
 export {host, transport}
