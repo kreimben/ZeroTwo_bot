8ec8af88ba0f3cc1553f645238ff0a706dfcf10b
Aksidion Kreimben <aksidion@kreimben.com>
2023-04-30 21:16:22 +0900

    Fix host address

--- pages/src/api/init.ts
+++ pages/src/api/init.ts
@@ -1,6 +1,6 @@
 import {GrpcWebFetchTransport} from "@protobuf-ts/grpcweb-transport";
 
 // @ts-ignore
-const host = import.meta.env.VITE_GRPC_HOST;
+const host = import.meta.env.VITE_GRPC_HOST || 'http://localhost:5012';
 const transport = new GrpcWebFetchTransport({baseUrl: host})
 export {host, transport}
