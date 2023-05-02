b06dc56776ac7511b5a523e9410bfb05f7c64982
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-02 16:18:49 +0900

    Implement error page.

--- connect/views.py
+++ connect/views.py
@@ -6,4 +6,12 @@ class ConnectView(BaseMenuView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['guild_id'] = self.request.GET.get('guild_id')
+        context['user_id'] = self.request.GET.get('user_id')
         return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if not context.get('guild_id', None) or not context.get('user_id', None):
+            self.template_name = 'connect_error.html'
+        return self.render_to_response(context)
