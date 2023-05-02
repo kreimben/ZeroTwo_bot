dbff0e70ba21aad6db0959d22f2680e0d5780da1
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-02 16:04:22 +0900

    Refactoring.

--- connect/forms.py
+++ connect/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 
-class CurrentQueueForm(forms.Form):
+class GuildAndUserForm(forms.Form):
+    """
+    This is basic form for required guild_id and user_id.
+    """
     guild_id = forms.CharField(max_length=20, min_length=5)
     user_id = forms.CharField(max_length=20, min_length=5)
